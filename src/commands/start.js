ef633ef0c2181a7955017ee267bbb6a88906d596
Luis de Leon <luyzdeleon@gmail.com>
2018-05-04 10:12:11 -0500

    Added network option to start command

--- src/commands/start.js
+++ src/commands/start.js
@@ -5,7 +5,8 @@ module.exports = function(program) {
     .command('start')
     .option('-p, --port [number]', 'Specify port [3000]', '3000')
     .option('-e, --ethereum [value]', 'Ethereum RPC-API host and port [http://localhost:8545]', 'http://localhost:8545')
+    .option('-n, --network [number]', 'Ethereum Network ID [4]', '4')
     .action(function (cmd) {
-      new PocketApiServer(cmd.port);
+      new PocketApiServer(cmd.port, cmd.ethereum, cmd.network);
     });
 }
